9ed0d8731dbefb46078d2e1d4118498970e38449
jessicas16 <101440484+jessicas16@users.noreply.github.com>
2023-05-26 09:24:32 +0700

    show pending, approve, show all (calvin yg kerja)

--- src/controllers/scheduleController.js
+++ src/controllers/scheduleController.js
@@ -7,6 +7,8 @@ const jwt = require("jsonwebtoken");
 const upload = multer({
   dest : "./uploads",
 });
+
+//====================================
 function keDate(dateString) {
   // Membagi string tanggal menjadi komponen hari, bulan, dan tahun
   const [day, month, year] = dateString.split('/');
@@ -24,6 +26,12 @@ function keTime(timeString) {
 
   return timeString;
 }
+
+function tanggalToString(tanggal) {
+  return (tanggal.toISOString().slice(0, 10).replace('T', ' '))
+}
+
+//====================================
 module.exports = {
   cekToken : async function (req, res, next){
     const token = req.headers["x-auth-token"];
@@ -42,7 +50,6 @@ module.exports = {
       return res.status(400).send(e);
     }
   },
-
   addSchedule : async function(req, res){
     const { username, date, time } = req.body;
     userLogin = req.user.id;
@@ -71,8 +78,12 @@ module.exports = {
       else {
         user2 = cariUser[0].dataValues;
 
+        if(user1.id == user2.id){
+          res.status(404).json({
+            "message" : "You can't add yourself"
+          })
+        }
 
-        
         const cariSchedule = await db.Schedule.findAll({
           where: {
             UserId1: user1.id,
@@ -96,8 +107,6 @@ module.exports = {
           })
           const result = {
             "message" : "Schedule Added",
-            // date : date,
-            // time : time,
             "scheduleId" : schedule.id,
             "UserId1" : user1.username,
             "UserId2" : user2.username,
@@ -173,6 +182,105 @@ module.exports = {
           }
           res.status(400).json(result);
         }
+      }
+    }
+  },
+  showPendingSchedule : async function(req, res){
+    const cariSchedule = await db.Schedule.findAll({
+      where: {
+        status: "pending"
+      }
+    })
+    const userLogin = req.user.id;
+    user1 = await db.User.findByPk(userLogin);
+    user1 = user1.dataValues;
+    if (cariSchedule.length == 0){
+      const result = {
+        "message" : "There are no pending schedule"
+      }
+      res.status(400).json(result);
+    }
+    else {
+
+      let temp = [];
+      for (let i = 0; i < cariSchedule.length; i++) {
+        if (user1.id == cariSchedule[i].UserId1 || user1.id == cariSchedule[i].UserId2){
+          temp.push({
+            id: cariSchedule[i].id,
+            tanggal: tanggalToString(cariSchedule[i].tanggal),
+            waktu: cariSchedule[i].waktu,
+          })
+        }
+      }
+
+      const result = {
+        "Schedule" : temp
+      }
+      
+      res.status(200).json(result);
+    }
+  },
+  listSchedule : async function(req, res){
+    const cariSchedule = await db.Schedule.findAll();
+    userLogin = req.user.id;
+    user1 = await db.User.findByPk(userLogin);
+    user1 = user1.dataValues;
+    
+    let temp = [];
+    for (let i = 0; i < cariSchedule.length; i++) {
+      if (user1.id==cariSchedule[i].UserId1||user1.id==cariSchedule[i].UserId2){
+        temp.push({
+          id: cariSchedule[i].id,
+          status: cariSchedule[i].status,
+          tanggal: tanggalToString(cariSchedule[i].tanggal),
+          waktu: cariSchedule[i].waktu,
+        })
+      }
+    }
+
+    const result = {
+      "List Schedule" : temp
+    }
+    res.status(200).json(result);
+  },
+  approveSchedule : async function(req, res){
+    const { id_schedule, status } = req.body;
+    userLogin = req.user.id;
+    user1 = await db.User.findByPk(userLogin);
+    user1 = user1.dataValues;
+    const cariSchedule = await db.Schedule.findAll({
+      where: {
+        id: req.body.id_schedule,
+        status: "pending"
+      }
+    })
+
+    if (cariSchedule.length == 0){
+      const result = {
+        "message" : "Schedule not found"
+      }
+      res.status(404).json(result);
+    }
+    else {
+      if(user1.id == cariSchedule[0].UserId2){
+        const updateStatus = await db.Schedule.update({
+          status: req.body.status
+        },{
+          where: {
+            id: req.body.id_schedule
+          }
+        })
+
+        const result = {
+          "message" : "Schedule Approved",
+        }
+        res.status(200).json(result);
+      }
+      else {
+        const result = {
+          "message" : "Invalid User"
+        }
+        res.status(400).json(result);
       }
     }
   }
--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -4,5 +4,8 @@ const router = Router();
 
 router.post("/add", scheduleController.cekToken, scheduleController.addSchedule);
 router.put("/cancel", scheduleController.cekToken, scheduleController.cancelSchedule);
+router.get("/list", scheduleController.cekToken, scheduleController.listSchedule);
+router.get("/pending", scheduleController.cekToken, scheduleController.showPendingSchedule);
+router.put("/approve", scheduleController.cekToken, scheduleController.approveSchedule);
 
 module.exports = router;
